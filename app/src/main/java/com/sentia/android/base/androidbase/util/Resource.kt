5cf0d5cdc4f278a4c445d145e98258a78754c90a
mario lopez <mario.lopez@sentia.com.au>
2017-12-28 16:09:18 +1100

    alternative implementation for ResourceResult

--- app/src/main/java/com/sentia/android/base/androidbase/util/Resource.kt
+++ app/src/main/java/com/sentia/android/base/androidbase/util/Resource.kt
@@ -6,7 +6,7 @@ import com.sentia.android.base.androidbase.util.exception.AppException
 /**
  * Created by mariolopez on 27/12/17.
  */
-class Resource<T> internal constructor(val status: Resource.Status, val data: T? = null, val exception: AppException? = null) {
+class Resource<out T> internal constructor(val status: Resource.Status, val data: T? = null, val exception: AppException? = null) {
     enum class Status {
         SUCCESS, ERROR, LOADING
     }
@@ -25,3 +25,14 @@ class Resource<T> internal constructor(val status: Resource.Status, val data: T?
         }
     }
 }
+//Alternative implementation
+sealed class  ResourceResult<out T>(val status: Resource.Status, val data: T? = null, val exception: AppException? = null)
+
+data class Success<T>(val dataSuccess: T? = null, val exceptionSucc: AppException? = null)
+    : ResourceResult<T>(SUCCESS, dataSuccess,exceptionSucc)
+
+data class Error<T>( val exceptionError: AppException? = null)
+    : ResourceResult<T>(SUCCESS, null,exceptionError)
+
+data class Loading<T>(val statusLoading:Resource.Status= LOADING)
+    : ResourceResult<T>(LOADING, null,null)
