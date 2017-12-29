e5acb3ce62f553ac2bb52a6f970bee5f801c2c81
mario lopez <mariolopez@sentia-imac.home.gateway>
2017-12-29 12:27:39 +1100

    added dependecy

--- app/src/main/java/com/sentia/android/base/androidbase/MainActivity.kt
+++ app/src/main/java/com/sentia/android/base/androidbase/MainActivity.kt
@@ -1,15 +1,16 @@
 package com.sentia.android.base.androidbase
 
+import android.arch.lifecycle.LifecycleOwner
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import com.github.salomonbrys.kodein.instance
 import com.sentia.android.base.androidbase.api.RestAdapter
-import com.sentia.android.base.androidbase.util.RxBus
 import com.sentia.android.base.androidbase.api.model.SampleBindingModel
 import com.sentia.android.base.androidbase.base.BaseActivity
 import com.sentia.android.base.androidbase.databinding.ActivityMainBinding
+import com.sentia.android.base.androidbase.util.RxBus
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseActivity(),LifecycleOwner {
 
     val rxBus by kodein.instance<RxBus>()
     val restAdapter by kodein.instance<RestAdapter>()
