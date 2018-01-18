e3f5591cb536d5d8280c0a2bf97380bcabc1a296
mario lopez <mariolopez@sentia-imac.home.gateway>
2018-01-18 15:36:32 +1100

    util dialog for uncomitted changes

--- app/src/main/java/com/sentia/android/base/vis/evaluation/EvaluationActivity.kt
+++ app/src/main/java/com/sentia/android/base/vis/evaluation/EvaluationActivity.kt
@@ -2,11 +2,13 @@ package com.sentia.android.base.vis.evaluation
 
 import android.arch.lifecycle.LifecycleOwner
 import android.os.Bundle
+import android.view.MenuItem
 import com.sentia.android.base.vis.R
 import com.sentia.android.base.vis.base.BaseActivity
 import com.sentia.android.base.vis.util.KEY_INSPECTION_ID
 import com.sentia.android.base.vis.util.tintBackArrow
 import kotlinx.android.synthetic.main.activity_evaluation.*
+import org.jetbrains.anko.alert
 
 
 class EvaluationActivity : BaseActivity(), LifecycleOwner {
@@ -21,10 +23,27 @@ class EvaluationActivity : BaseActivity(), LifecycleOwner {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.tintBackArrow(R.color.white)
 
-
         tl_evaluation.setupWithViewPager(vp_tabs_evaluation)
-        vp_tabs_evaluation.adapter = EvaluationTabsPagerAdapter(supportFragmentManager, resources,intent.getLongExtra(KEY_INSPECTION_ID,0L))
+        vp_tabs_evaluation.adapter = EvaluationTabsPagerAdapter(supportFragmentManager, resources, intent.getLongExtra(KEY_INSPECTION_ID, 0L))
 
     }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            android.R.id.home -> {
+                alert {
+                    title = getString(R.string.alert_attention)
+                    negativeButton(android.R.string.cancel, { it.dismiss() })
+                    positiveButton(android.R.string.ok, {
+                        it.dismiss()
+                        super.onBackPressed()
+                    })
+                    message = getString(R.string.alert_warning_closing)
+
+                }.show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
